4b0993876948036fed2231c92581161411b91c39
Den <52295780+josserden@users.noreply.github.com>
2022-10-14 11:20:56 +0300

    remove edit form component

--- src/components/EditForm/EditForm.jsx
+++
@@ -1,9 +0,0 @@
-import { RiSaveLine } from 'react-icons/ri';
-import { MdOutlineCancel } from 'react-icons/md';
-
-import { SearchFormStyled, FormBtn, InputSearch } from 'components';
-import { BtnEdit } from './EditForm.styled';
-
-export const EditForm = () => {
-  return <h2>Edit Form</h2>;
-};
