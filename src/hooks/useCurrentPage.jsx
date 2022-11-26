7a2f9e39e50e1f0fb30424aa614f4ae8a113bdf4
Den <52295780+josserden@users.noreply.github.com>
2022-11-26 11:54:47 +0200

    initial commit

--- src/hooks/useCurrentPage.jsx
+++
@@ -1,8 +0,0 @@
-import {useLocation} from "react-router-dom";
-import {routes} from "../routes.js";
-
-export const useCurrentPage = () => {
-    const {pathname} = useLocation();
-    const isHomePage = pathname === routes.HOME;
-    return { isHomePage };
-}
